ffc7edc04f965f22ff0f67287487f175bf19c936
franilones <franolmosdev@gmail.com>
2024-05-03 20:41:53 +0200

    Configuración para poder conectarse a la base de datos con sequelize, module-alias y dotenv

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+require('module-alias/register');
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -12,10 +15,19 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+// !!!!!!! Para comprobar que se conecta bien a la base de datos !!!!!!
+// const sequelize = require('@configs/dbConfig');
+//
+// sequelize.authenticate()
+//     .then(() => {
+//         console.log('Conexión establecida correctamente.');
+//     })
+//     .catch(err => {
+//         console.error('No se pudo conectar a la base de datos:', err);
+//     });
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,19 +35,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
