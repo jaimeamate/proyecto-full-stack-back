83c3cc0d079795f8142e3e55593d80f770be4075
jdeking14 <>
2024-06-11 18:01:10 +0200

    fix del hook al cambiar password

--- models/userModel.js
+++ models/userModel.js
@@ -38,7 +38,7 @@ const User = sequelize.define("User", {
     hooks:{
         // Antes de guardar un usuario se comprueba si tiene pass y si la tiene la encripta.
         beforeSave: async (user) => {
-            if(user.password){
+            if(user.changed('password')){
                 // Genera una cadena aleatoria que se utiliza para alterar el proceso de cifrado
                 // Evita ataques de fuerza bruta dos contraseñas idénticas darán hashes diferentes
                 const salt = await bcrypt.genSalt(10);
