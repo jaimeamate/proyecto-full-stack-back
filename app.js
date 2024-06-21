7c5af912312220fc5e269b2e249d0ff8edc2e54c
Franilones <franolmosdev@gmail.com>
2024-06-21 23:53:17 +0200

    Envío de inivitación cuando se añade un usuario

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-require("module-alias/register");
-require("dotenv").config();
+require('module-alias/register');
+require('dotenv').config();
 
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 
 const bodyParser = require('body-parser');
@@ -18,20 +21,25 @@ const apiRouter = require('@routes/api');
 
 const port = process.env.PORT || 3000;
 
+const cors = require('cors');
 
 
 //const port1 = process.env.PORT1 || 3001;
 
 app.use(cors());
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', apiRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 
 
@@ -56,11 +64,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 
@@ -70,3 +78,4 @@ app.use(function (err, req, res, next) {
 //});
 
 module.exports = app;
+
--- services/userHasGroupServices.js
+++ services/userHasGroupServices.js
@@ -1,106 +1,156 @@
 const {
-  UsersHasGroups,
-  User,
-  Group,
-  Activity,
-  sequelize,
+    UsersHasGroups,
+    User,
+    Group,
+    Activity,
+    sequelize,
 } = require("@models/index");
-const { getGroupWithId } = require("./groupService");
-const { getUserWithId } = require("./userService");
-const { Sequelize, Op } = require("sequelize");
+
+const {getGroupWithId} = require("./groupService");
+const {getUserWithId} = require("./userService");
+const {Sequelize, Op} = require("sequelize");
+const nodemailer = require("nodemailer");
+const path = require("path");
+const ejs = require("ejs");
+require('dotenv').config();
+
+let transporter = nodemailer.createTransport({
+    service: 'outlook',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+const sendEmail = (to, subject, htmlContent) => {
+    let mailOptions = {
+        from: process.env.EMAIL_USER,
+        to,
+        subject,
+        html: htmlContent
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.error('Error al enviar el correo:', error);
+        }
+        console.log('Correo enviado:', info.response);
+    });
+};
+
+const renderHtml = async (userEmail, groupName) => {
+    try {
+        let templatePath = path.join(__dirname, '../html', 'invitation.ejs');
+        console.log('Template Path:', templatePath); // Depuración
+        const html = await ejs.renderFile(templatePath, {userEmail, groupName});
+        console.log('HTML Rendered:', html); // Depuración
+        return html;
+    } catch (error) {
+        console.error('Error al renderizar el HTML:', error);
+        return '<p>Error loading HTML content</p>';
+    }
+};
 
 const register_User_has_Group = async (userIds, GroupId) => {
-  try {
-    if (!Array.isArray(userIds) || !GroupId) {
-      throw new Error("Invalid input");
+    try {
+        if (!Array.isArray(userIds) || !GroupId) {
+            throw new Error("Invalid input");
+        }
+
+        const hasgroup = await Group.findByPk(GroupId);
+
+        if (!hasgroup) {
+            throw new Error("Group not found");
+        }
+
+        // Verifica que todos los usuarios existen
+        const users = await User.findAll({
+            where: {
+                id: userIds,
+            },
+        });
+
+        if (users.length !== userIds.length) {
+            throw new Error("Some users not found");
+        }
+
+        for (const user of users) {
+            const email = user.email;
+            const groupName = hasgroup.name;
+            const htmlContent = await renderHtml(email, groupName);
+            console.log('Sending email to:', email, 'with content:', htmlContent); // Depuración
+            sendEmail(email, `Invitación a grupo ${hasgroup.name}`, htmlContent);
+        }
+
+        const userGroups = userIds.map((userId) => ({
+            idUser: userId,
+            idGroup: GroupId,
+        }));
+
+        return await UsersHasGroups.bulkCreate(userGroups);
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
-
-    const hasgroup = await Group.findByPk(GroupId);
-
-    if (!hasgroup) {
-      throw new Error("Group not found");
-    }
-
-    // Verifica que todos los usuarios existen
-    const users = await User.findAll({
-      where: {
-        id: userIds,
-      },
-    });
-
-    if (users.length !== userIds.length) {
-      throw new Error("Some users not found");
-    }
-
-    const userGroups = userIds.map((userId) => ({
-      idUser: userId,
-      idGroup: GroupId,
-    }));
-
-    return await UsersHasGroups.bulkCreate(userGroups);
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
 };
 
 const find_Users_of_Group = async (groupId) => {
-  try {
-    if (!groupId) {
-      throw new Error("Invalid input");
+    try {
+        if (!groupId) {
+            throw new Error("Invalid input");
+        }
+
+        const groups = await getGroupWithId(groupId);
+
+        if (!groups) {
+            throw new Error("Group not found");
+        }
+
+        return await UsersHasGroups.findAll({
+            where: {
+                idGroup: groupId,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
-
-    const groups = await getGroupWithId(groupId);
-
-    if (!groups) {
-      throw new Error("Group not found");
-    }
-
-    return await UsersHasGroups.findAll({
-      where: {
-        idGroup: groupId,
-      },
-    });
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
 };
 
 const find_Groups_of_User = async (userId) => {
-  try {
-    if (!userId) {
-      throw new Error("Invalid input");
+    try {
+        if (!userId) {
+            throw new Error("Invalid input");
+        }
+
+        const user = await getUserWithId(userId);
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        return await UsersHasGroups.findAll({
+            where: {
+                idUser: userId,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
-
-    const user = await getUserWithId(userId);
-
-    if (!user) {
-      throw new Error("User not found");
-    }
-
-    return await UsersHasGroups.findAll({
-      where: {
-        idUser: userId,
-      },
-    });
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
 };
 
 const finally_Find_Groups = async (id_Of_Groups) => {
-  try {
-    return await Group.findAll({
-      where: {
-        id: id_Of_Groups,
-      },
-    });
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+    try {
+        return await Group.findAll({
+            where: {
+                id: id_Of_Groups,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 };
 
 /*const finally_Find_Users = async (id_Of_Users) => {
@@ -117,222 +167,222 @@ const finally_Find_Groups = async (id_Of_Groups) => {
 };*/
 
 const finally_Find_Users = async (id_Of_Users) => {
-  try {
-    return await User.findAll({
-      where: {
-        id: {
-          [Op.in]: id_Of_Users,
-        },
-      },
-      attributes: ["id", "firstName", "lastName", "email"], // Me traigo solo esto
-    });
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+    try {
+        return await User.findAll({
+            where: {
+                id: {
+                    [Op.in]: id_Of_Users,
+                },
+            },
+            attributes: ["id", "firstName", "lastName", "email"], // Me traigo solo esto
+        });
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 };
 
 const admin_Find_Users = async (result_Users, AdminIs) => {
-  const usersWithIsAdmin = result_Users.map((user) => ({
-    ...user.dataValues,
-    isAdmin: false, // Aquí defines la lógica para determinar isAdmin
-  }));
+    const usersWithIsAdmin = result_Users.map((user) => ({
+        ...user.dataValues,
+        isAdmin: false, // Aquí defines la lógica para determinar isAdmin
+    }));
 
-  return usersWithIsAdmin.map((user) => ({
-    ...user,
-    isAdmin: user.id === AdminIs,
-  }));
+    return usersWithIsAdmin.map((user) => ({
+        ...user,
+        isAdmin: user.id === AdminIs,
+    }));
 };
 
 const register_Admin = async (name) => {
-  try {
-    const newUserHGroup = new UsersHasGroups(name);
-    return await newUserHGroup.save();
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
+    try {
+        const newUserHGroup = new UsersHasGroups(name);
+        return await newUserHGroup.save();
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
 };
 
 const patch_Users_Has_Groups = async (groupId, usersIn, usersOut) => {
-  try {
-    if (!Array.isArray(usersOut) || !Array.isArray(usersIn) || !groupId) {
-      throw new Error("Invalid input");
+    try {
+        if (!Array.isArray(usersOut) || !Array.isArray(usersIn) || !groupId) {
+            throw new Error("Invalid input");
+        }
+
+        const hasgroup = await Group.findByPk(groupId);
+
+        if (!hasgroup) {
+            throw new Error("Group not found");
+        }
+
+        // Verifica que todos los usuarios para ingresar existan
+        const inUsers = await User.findAll({
+            where: {
+                id: usersIn,
+            },
+        });
+
+        if (inUsers.length !== usersIn.length) {
+            throw new Error("Some users to insert not found");
+        }
+
+        // Verifica que todos los usuarios para sacar existan
+        const outUsers = await UsersHasGroups.findAll({
+            where: {
+                idUser: {
+                    [Sequelize.Op.in]: usersOut,
+                },
+                idGroup: groupId,
+            },
+        });
+
+        if (outUsers.length !== usersOut.length) {
+            throw new Error("Some users to delete not found");
+        }
+
+        const userToInsert = usersIn.map((userId) => ({
+            idUser: userId,
+            idGroup: groupId,
+        }));
+        const out = await out_Users(usersOut, groupId);
+        return await UsersHasGroups.bulkCreate(userToInsert);
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
-
-    const hasgroup = await Group.findByPk(groupId);
-
-    if (!hasgroup) {
-      throw new Error("Group not found");
-    }
-
-    // Verifica que todos los usuarios para ingresar existan
-    const inUsers = await User.findAll({
-      where: {
-        id: usersIn,
-      },
-    });
-
-    if (inUsers.length !== usersIn.length) {
-      throw new Error("Some users to insert not found");
-    }
-
-    // Verifica que todos los usuarios para sacar existan
-    const outUsers = await UsersHasGroups.findAll({
-      where: {
-        idUser: {
-          [Sequelize.Op.in]: usersOut,
-        },
-        idGroup: groupId,
-      },
-    });
-
-    if (outUsers.length !== usersOut.length) {
-      throw new Error("Some users to delete not found");
-    }
-
-    const userToInsert = usersIn.map((userId) => ({
-      idUser: userId,
-      idGroup: groupId,
-    }));
-    const out = await out_Users(usersOut, groupId);
-    return await UsersHasGroups.bulkCreate(userToInsert);
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
 };
 
 const out_Users = async (usersOut, groupId) => {
-  const result = await UsersHasGroups.destroy({
-    where: {
-      idUser: {
-        [Sequelize.Op.in]: usersOut,
-      },
-      idGroup: groupId,
-    },
-  });
-  return result;
+    const result = await UsersHasGroups.destroy({
+        where: {
+            idUser: {
+                [Sequelize.Op.in]: usersOut,
+            },
+            idGroup: groupId,
+        },
+    });
+    return result;
 };
 
 const find_Activity_of_Group = async (groupId) => {
-  try {
-    if (!groupId) {
-      throw new Error("Invalid input");
+    try {
+        if (!groupId) {
+            throw new Error("Invalid input");
+        }
+
+        const groups = await getGroupWithId(groupId);
+
+        if (!groups) {
+            throw new Error("Group not found");
+        }
+
+        return await Activity.findAll({
+            where: {
+                idGroup: groupId,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
-
-    const groups = await getGroupWithId(groupId);
-
-    if (!groups) {
-      throw new Error("Group not found");
-    }
-
-    return await Activity.findAll({
-      where: {
-        idGroup: groupId,
-      },
-    });
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
 };
 
 const findAdminIs = async (groupId) => {
-  try {
-    const hasgroup = await Group.findByPk(groupId);
+    try {
+        const hasgroup = await Group.findByPk(groupId);
 
-    if (!hasgroup) {
-      throw new Error("Group not found");
+        if (!hasgroup) {
+            throw new Error("Group not found");
+        }
+        const adminUser = await UsersHasGroups.findOne({
+            where: {
+                idGroup: groupId,
+                isAdmin: true,
+            },
+        });
+
+        return (adminIsHere = JSON.stringify(adminUser.dataValues));
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
-    const adminUser = await UsersHasGroups.findOne({
-      where: {
-        idGroup: groupId,
-        isAdmin: true,
-      },
-    });
-
-    return (adminIsHere = JSON.stringify(adminUser.dataValues));
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
 };
 
 const delete_User_has_Group = async (usersOut, groupId) => {
-  const usersToDel = await UsersHasGroups.findAll({
-    where: {
-      idUser: {
-        [Sequelize.Op.in]: usersOut,
-      },
-      idGroup: groupId,
-    },
-  });
+    const usersToDel = await UsersHasGroups.findAll({
+        where: {
+            idUser: {
+                [Sequelize.Op.in]: usersOut,
+            },
+            idGroup: groupId,
+        },
+    });
 
-  if (usersToDel.length !== usersOut.length) {
-    throw new Error("Some users to delete not found");
-  }
+    if (usersToDel.length !== usersOut.length) {
+        throw new Error("Some users to delete not found");
+    }
 
-  const out = await out_Users(usersOut, groupId);
+    const out = await out_Users(usersOut, groupId);
 };
 
 const change_Admin_Has_Group = async (newAdmin, oldAdmin, groupId) => {
-  const userAdm = [newAdmin, oldAdmin];
+    const userAdm = [newAdmin, oldAdmin];
 
-  const admins = await UsersHasGroups.findAll({
-    where: {
-      idUser: {
-        [Sequelize.Op.in]: userAdm,
-      },
-      idGroup: groupId,
-    },
-  });
-  //---
+    const admins = await UsersHasGroups.findAll({
+        where: {
+            idUser: {
+                [Sequelize.Op.in]: userAdm,
+            },
+            idGroup: groupId,
+        },
+    });
+    //---
 
-  if (admins.length !== userAdm.length) {
-    throw new Error("Some users not found");
-  }
-  try {
-    const transaction = await sequelize.transaction();
-    // Actualiza el antiguo administrador a no administrador
-    await UsersHasGroups.update(
-      { isAdmin: false },
-      {
-        where: { idUser: oldAdmin },
-        transaction,
-      }
-    );
+    if (admins.length !== userAdm.length) {
+        throw new Error("Some users not found");
+    }
+    try {
+        const transaction = await sequelize.transaction();
+        // Actualiza el antiguo administrador a no administrador
+        await UsersHasGroups.update(
+            {isAdmin: false},
+            {
+                where: {idUser: oldAdmin},
+                transaction,
+            }
+        );
 
-    // Actualiza el nuevo administrador a administrador
-    await UsersHasGroups.update(
-      { isAdmin: true },
-      {
-        where: { idUser: newAdmin },
-        transaction,
-      }
-    );
+        // Actualiza el nuevo administrador a administrador
+        await UsersHasGroups.update(
+            {isAdmin: true},
+            {
+                where: {idUser: newAdmin},
+                transaction,
+            }
+        );
 
-    // Si todo va bien, confirma la transacción
-    await transaction.commit();
-    return { success: true, message: "Admin roles updated successfully" };
-  } catch (error) {
-    // Si hay un error, revierte la transacción
-    await transaction.rollback();
-    throw error;
-  }
+        // Si todo va bien, confirma la transacción
+        await transaction.commit();
+        return {success: true, message: "Admin roles updated successfully"};
+    } catch (error) {
+        // Si hay un error, revierte la transacción
+        await transaction.rollback();
+        throw error;
+    }
 };
 
 module.exports = {
-  register_User_has_Group,
-  find_Users_of_Group,
-  find_Groups_of_User,
-  register_Admin,
-  patch_Users_Has_Groups,
-  find_Activity_of_Group,
-  findAdminIs,
-  delete_User_has_Group,
-  change_Admin_Has_Group,
-  finally_Find_Groups,
-  finally_Find_Users,
-  admin_Find_Users,
+    register_User_has_Group,
+    find_Users_of_Group,
+    find_Groups_of_User,
+    register_Admin,
+    patch_Users_Has_Groups,
+    find_Activity_of_Group,
+    findAdminIs,
+    delete_User_has_Group,
+    change_Admin_Has_Group,
+    finally_Find_Groups,
+    finally_Find_Users,
+    admin_Find_Users,
 };
